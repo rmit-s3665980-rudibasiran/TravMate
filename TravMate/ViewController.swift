120b4c9a0adf7624812d7b65f667980e033ffc37
rudibasiran <s3665980@student.rmit.edu.au>
2018-09-12 18:38:19 +1000

    Initial Commit

---
+++ TravMate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravMate
+//
+//  Created by Rudi Basiran on 12/9/18.
+//  Copyright © 2018 RMIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

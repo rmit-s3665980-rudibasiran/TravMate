6d8b99d1eab659dec31f0e6af23cfb4b95116ef1
rudibasiran <s3665980@student.rmit.edu.au>
2018-09-14 04:18:57 +1000

    Tab Bar Icons

--- TravMate/Model/Trip.swift
+++ TravMate/Model/Trip.swift
@@ -9,22 +9,88 @@
 import Foundation
 import UIKit
 
-class Trip {
-    
-    var locationName = ["Melbourne", "Sydney", "Perth"]
-    var locationDesc = ["Victoria", "New South Wales", "Western Australia"]
-    var locationImage = [UIImage(named: "melbourne"), UIImage(named: "sydney"), UIImage (named: "perth")]
-    
-    var title: String?
-    var image: UIImage?
+enum Trip:Int
+{
+    case first=1, second, third, fourth
     
     init() {
+        self = .first
+    }
+    
+    init?(number:Int)
+    {
+        switch number {
+        case 1: self = .first
+        case 2: self = .second
+        case 3: self = .third
+        case 4: self = .fourth
+        default: return nil
+        }
+    }
+    
+    var rank:Int
+    {
+        get {
+            return self.rawValue
+        }
+    }
+    
+    var name:String
+    {
+        get {
+            switch self {
+            case .first:    return "First"
+            case .second:   return "Second"
+            case .third:    return "Third"
+            case .fourth:   return "Fourth"
+            }
+        }
+    }
+    
+    var imageName:String
+    {
+        get {
+            return self.name.replacingOccurrences(of: " ", with: "")
+        }
+    }
+    
+    var interpretation:String
+    {
+        get {
+            switch self {
+            case .first:    return "Melbourne"
+            case .second:   return "Perth"
+            case .third:    return "Sydney"
+            case .fourth:   return "Brisbane"
+            }
+        }
+    }
+    
+    static func getTrip() ->[Trip]
+    {
+        return [first, second, third, fourth]
+    }
+    
+    static func getTripDictionary() ->[String:Trip]
+    {
+        // Create a dictionary object to return to the caller
+        var allTrips:[String:Trip] = [:]
         
+        // Iterate through all the elements of the enum and add each trip
+        for trip in getTrip()
+        {
+            allTrips.updateValue(trip, forKey: trip.name)
+        }
+        return allTrips
     }
     
-    init(title: String, image: UIImage) {
-        self.title = title;
-        self.image = image;
+    func getTripDescription() ->String
+    {
+        let interpretationMinusFullStop = String(self.interpretation.dropLast())
+        
+        // Build the string description of the trip
+        var description = "My " + self.name
+        description +=  " Trip: " + interpretationMinusFullStop
+        return description
     }
-    
 }

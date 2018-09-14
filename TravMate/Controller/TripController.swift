657d43425a4479052fd926b706ec2bef9456ca6b
rudibasiran <s3665980@student.rmit.edu.au>
2018-09-14 12:09:09 +1000

    TimeStampOK

--- TravMate/Controller/TripController.swift
+++ TravMate/Controller/TripController.swift
@@ -11,26 +11,22 @@ import UIKit
 
 class TripController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
-    let locationName = ["Melbourne", "Sydney", "Perth"]
-    let locationDesc = ["Melbourne is the capital and most populous city of the Australian state of Victoria, and the second-most populous city in Australia and Oceania","Sydney is the state capital of New South Wales and the most populous city in Australia and Oceania. ","Perth is the capital and largest city of the Australian state of Western Australia"]
-    let locationImage = [UIImage(named: "melbourne"), UIImage(named: "sydney"), UIImage(named: "perth")]
-    
-    // var trip:Trip = Trip()
+    var trip:Trip = Trip()
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return locationName.count
+        return trip.getSizeofData()
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! TripCollectionViewCell
         
-        cell.tripName.text = locationName[indexPath.row]
-        cell.tripImage.image = locationImage[indexPath.row]
-        cell.tripDesc.text = locationDesc[indexPath.row]
+        cell.tripName.text = trip.locationName[indexPath.row]
+        cell.tripImage.image =  UIImage(named: trip.locationImage[indexPath.row]) 
+        cell.tripDesc.text = trip.locationDesc[indexPath.row]
         
         cell.contentView.layer.cornerRadius = 4.0
         cell.contentView.layer.borderWidth = 1.0
--- TravMate/Model/Trip.swift
+++ TravMate/Model/Trip.swift
@@ -9,81 +9,20 @@
 import Foundation
 import UIKit
 
-enum Trip:Int
-{
-    case first=1, second, third, fourth
+struct Trip {
+    
+    var locationName = ["London", "Melbourne", "Sydney", "Perth"]
+    var locationDesc = ["London is in England, the United Kingdom",
+                        "Melbourne is the capital and most populous city of the Australian state of Victoria, and the second-most populous city in Australia and Oceania",
+                        "Sydney is the state capital of New South Wales and the most populous city in Australia and Oceania.",
+                        "Perth is the capital and largest city of the Australian state of Western Australia"]
+    var locationImage = ["london", "melbourne", "sydney", "perth"]
     
     init() {
-        self = .first
     }
     
-    init?(number:Int)
-    {
-        switch number {
-        case 1: self = .first
-        case 2: self = .second
-        case 3: self = .third
-        case 4: self = .fourth
-        default: return nil
-        }
+    func getSizeofData() -> Int {
+        return locationName.count
     }
-    
-    var rank:Int
-    {
-        get {
-            return self.rawValue
-        }
-    }
-    
-    var name:String
-    {
-        get {
-            switch self {
-            case .first:    return "First"
-            case .second:   return "Second"
-            case .third:    return "Third"
-            case .fourth:   return "Fourth"
-            }
-        }
-    }
-    
-    var imageName:String
-    {
-        get {
-            return self.name.replacingOccurrences(of: " ", with: "")
-        }
-    }
-    
-    var interpretation:String
-    {
-        get {
-            switch self {
-            case .first:    return "Melbourne"
-            case .second:   return "Perth"
-            case .third:    return "Sydney"
-            case .fourth:   return "Brisbane"
-            }
-        }
-    }
-    
-    static func getTrip() ->[Trip]
-    {
-        return [first, second, third, fourth]
-    }
-    
-    static func getTripDictionary() ->[String:Trip]
-    {
-        var allTrips:[String:Trip] = [:]
-        for trip in getTrip() {
-            allTrips.updateValue(trip, forKey: trip.name)
-        }
-        return allTrips
-    }
-    
-    func getTripDescription() ->String {
-        let interpretationMinusFullStop = String(self.interpretation.dropLast())
-        var description = "My " + self.name
-        description +=  " Trip: " + interpretationMinusFullStop
-        return description
-    }
+
 }

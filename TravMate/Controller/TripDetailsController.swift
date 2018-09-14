d8bae805fe0ad8690a9bca8b6ecfbf80e5566113
rudibasiran <s3665980@student.rmit.edu.au>
2018-09-14 22:27:48 +1000

    SaveTripDesc

--- TravMate/Controller/TripDetailsController.swift
+++ TravMate/Controller/TripDetailsController.swift
@@ -14,10 +14,20 @@ class TripDetailsController: UIViewController {
     @IBOutlet weak var tripDetlName: UITextField!
     @IBOutlet weak var tripDetlDesc: UITextView!
     
+    @IBAction func saveData(_ sender: Any) {
+        
+        let tripDesc = tripDetlDesc.text
+        Trip.sharedInstance.locationDesc[Trip.sharedInstance.myCurrentTrip] = tripDesc!
+        
+        performSegue(withIdentifier: "SegueBackToTrip", sender: self)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         tripDetlName.text = Trip.sharedInstance.locationName[Trip.sharedInstance.myCurrentTrip]
         tripDetlDesc.text = Trip.sharedInstance.locationDesc[Trip.sharedInstance.myCurrentTrip]
        
     }
+    
+    
 }

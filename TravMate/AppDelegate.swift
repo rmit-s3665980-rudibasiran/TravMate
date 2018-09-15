1497fd9f23b6fa5e2989940f6c67b53d78f09bb2
wanyii <s3368772@student.rmit.edu.au>
2018-09-15 16:10:20 +1000

    updated

--- TravMate/AppDelegate.swift
+++ TravMate/AppDelegate.swift
@@ -12,9 +12,25 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+//    func uicolorFromHex(rgbValue:UInt32)->UIColor{
+//        let red = CGFloat((rgbValue & 0xFF0000) >> 16)/256.0
+//        let green = CGFloat((rgbValue & 0xFF00) >> 8)/256.0
+//        let blue = CGFloat(rgbValue & 0xFF)/256.0
+//        
+//        return UIColor(red:red, green:green, blue:blue, alpha:1.0)
+//    }
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+//        let navigationBarAppearance = UINavigationBar.appearance()
+//
+//        navigationBarAppearance.tintColor = uicolorFromHex(rgbValue: 0xffffff)
+//        navigationBarAppearance.barTintColor = uicolorFromHex(rgbValue: 0x000000)
+//        // change navigation item title color
+//        navigationBarAppearance.titleTextAttributes =  [kCTForegroundColorAttributeName as NSAttributedStringKey:UIColor.white]
+//        UIApplication.shared.statusBarStyle = UIStatusBarStyle.lightContent
+
         // Override point for customization after application launch.
         return true
     }

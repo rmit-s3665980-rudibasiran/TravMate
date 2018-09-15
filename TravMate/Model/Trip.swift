35f06b6a6623077b37bec8768751f2a6de407b09
rudibasiran <s3665980@student.rmit.edu.au>
2018-09-15 19:44:44 +1000

    Trip Details

--- TravMate/Model/Trip.swift
+++ TravMate/Model/Trip.swift
@@ -14,7 +14,7 @@ struct Trip {
     var myCurrentTrip = 0;
 
     var locationName = ["LONDON, ENGLAND", "NEW YORK, USA", "PARIS, FRANCE", "MEBOURNE, AUSTRALIA"]
-    var locationDays = ["10 days", "11 days", "14 days", "5 days"]
+    var locationDays = ["10 Days", "11 Days", "14 Days", "5 Days"]
     var locationCost = ["$5,000" , "$7,000", "$8,000", "$500" ]
     
     var flight:Flight = Flight ()

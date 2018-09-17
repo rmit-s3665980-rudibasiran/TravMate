42106f17b40f7a906e2533ab259f88476fb89714
wanyii <s3368772@student.rmit.edu.au>
2018-09-17 14:14:19 +1000

    tabbycontroller
    reminder: to create enums for each tabs to control views to flight, hotel, restaurant

---
+++ TravMate/Controller/TabbyController.swift
@@ -0,0 +1,39 @@
+//
+//  TabbyController.swift
+//  TravMate
+//
+//  Created by wanyii on 17/9/18.
+//  Copyright © 2018 RMIT. All rights reserved.
+//
+
+import UIKit
+
+class TabbyController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.selectedIndex = 2
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
